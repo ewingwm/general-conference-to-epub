3cbc51c6824f9a19cffc2e7ea0353748aa5f7a5b
ewingwm <ewingwm@gmail.com>
2025-05-18 17:39:41 -0400

    Created Link List

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
+blink = 'https://www.churchofjesuschrist.org/study/general-conference/2025/04'
 # Make request for index page
-index = requests.get('https://www.churchofjesuschrist.org/study/general-conference/2025/04?lang=eng')
+index = requests.get(blink + '?lang=eng')
 # Make Soup
 index_soup = BeautifulSoup(index.text, 'html.parser')
 # print(index_soup.prettify())
 
 # Find all the links
 links_soup = index_soup.find_all('a')
+links = []
 # print(links_soup)
 for link in links_soup:
     name_soup = link.find_all('p', class_='subtitle-LKtQp')
@@ -18,8 +20,8 @@ for link in links_soup:
         name = name.split('>')
         name = name[1]
         name = name.split()
-        name = name[length(name - 1]
+        name = name[len(name) - 1]
         name = name.split('<')
         name = name[0]
-        links.append(link.get('href'))
-
+        flink = 'https://www.churchofjesuschrist.org' + link.get('href')
+        links.append(flink)
