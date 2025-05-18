b0b824d60fd4e953e42ba69e49f3f6d2d19f4b2f
ewingwm <ewingwm@gmail.com>
2025-05-18 17:34:48 -0400

    Found Name and Link

--- main.py
+++ main.py
@@ -4,6 +4,22 @@ from bs4 import BeautifulSoup
 # Make request for index page
 index = requests.get('https://www.churchofjesuschrist.org/study/general-conference/2025/04?lang=eng')
 # Make Soup
-index_soup = BeautifulSoup(index.text)
+index_soup = BeautifulSoup(index.text, 'html.parser')
 # print(index_soup.prettify())
+
+# Find all the links
+links_soup = index_soup.find_all('a')
+# print(links_soup)
+for link in links_soup:
+    name_soup = link.find_all('p', class_='subtitle-LKtQp')
+    # print(name_soup)
+    if(name_soup != []):
+        name = str(name_soup)
+        name = name.split('>')
+        name = name[1]
+        name = name.split()
+        name = name[length(name - 1]
+        name = name.split('<')
+        name = name[0]
+        links.append(link.get('href'))
 
